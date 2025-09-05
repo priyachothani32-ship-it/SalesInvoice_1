9c6f1f53bf2fffad25813f20620be1ae8dd19782
priyachothani32-ship-it <priyachothani32@gmail.com>
2025-09-05 13:52:55 +0530

    Create CommClass.cs

---
+++ CommClass.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseConfiguration
+{
+    //shared/global class used to pass data between forms.
+    public class CommClass
+    {
+        public static string Connection;  
+        public static string filePath;
+      
+        //public string Connection { get; set; }
+    }
+}
