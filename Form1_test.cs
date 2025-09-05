979f9b5a7d45d559aa0bd1819e6346c3e2533e19
priyachothani32-ship-it <priyachothani32@gmail.com>
2025-09-05 13:56:05 +0530

    Create Form1_test.cs

---
+++ Form1_test.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseConfiguration
+{
+    public partial class Form1_test : Form
+    {
+        public Form1_test()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbProductCode_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbProductName_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_test_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
